e9a35df148d31c8219926551a42f26ee03cdb058
AleStaffa99 <alessandro.staffaroni@gmail.com>
2025-04-15 23:17:54 +0200

    modified odometer.cpp

--- first_project/src/odometer.cpp
+++ first_project/src/odometer.cpp
@@ -2,6 +2,7 @@
 #include "nav_msgs/Odometry.h"
 #include "geometry_msgs/PointStamped.h"
 #include <tf/transform_broadcaster.h>
+#include<geometry_msgs/Pose2D.h>
 
 class Odometer
 {
@@ -9,6 +10,7 @@ private:
 
     //Node handle object
     ros::NodeHandle nh;
+    ros::NodeHandle private_nh;
 
     //tf and Odometry messages
     tf::TransformBroadcaster br;
@@ -28,6 +30,10 @@ private:
     double y;
     double theta;
 
+    //thresholds
+    double steer_threshold;
+    double speed_threshold;
+
     //parameters
     double wheelbase;
     int scale_factor;
@@ -40,29 +46,34 @@ private:
         double steer = msg->point.x; //in grad
         steer = (steer/this->scale_factor) * M_PI / 180; //convert it in grad
         
-        if((std::abs(steer) < 0.005) || (speed < 0.1)) steer = 0;
-        //compute time slice 
+        //add threshold to steering noise
+        if((std::abs(steer) < 0.002) || (speed < 0.1)) steer = 0;
+
+        //setup current time  
         ros::Time current_time = msg->header.stamp;
 
+        //initialize last time
         if (this->last_time.isZero()){
             this->last_time = current_time;
             return;
         }
+
+        //compute delta t
         double dt = (current_time - this->last_time).toSec();
 
+        //checks on dt
         if(dt <= 0 || dt > 1) return;
 
         //bicycle kinematics
         double omega = speed * tan(steer) / this->wheelbase;
-
-        //Runge-Kutta integration
-        double dx = speed * dt * cos(this->theta + (omega * dt / 2));
-        double dy = speed * dt * sin(this->theta + (omega * dt / 2));
         double dtheta = dt * omega;
 
-        this->x += dx;
-        this->y += dy;
-        this->theta += dtheta;
+        //computes integration
+        integrate(omega, speed, dt, dtheta, this->x, this->y, this->theta);
+
+        // Normalize
+        while (theta > M_PI) theta -= 2 * M_PI;
+        while (theta < -M_PI) theta += 2 * M_PI;
         
         //Odometry message
         this->odom_msg.header.stamp = current_time;
@@ -72,21 +83,39 @@ private:
         this->odom_msg.pose.pose.position.y = this->y;
         this->odom_msg.pose.pose.position.z = 0.0;
         this->odom_msg.pose.pose.orientation = tf::createQuaternionMsgFromYaw(this->theta);
-
         this->odom_msg.twist.twist.linear.x = speed;
         this->odom_msg.twist.twist.angular.z = dtheta / dt;
-
         this->pub.publish(this->odom_msg);
         
         //Tf
         this->tr.setOrigin(tf::Vector3(this->x, this->y, 0));
         this->q.setRPY(0, 0, this->theta);
         this->tr.setRotation(q);
-
         this->br.sendTransform(tf::StampedTransform(tr, current_time, "world", "odom"));
 
         //Update last time 
         this->last_time = current_time;
+    }
+
+    void integrate(double omega, double speed,  double dt, double dtheta, double &x, double &y, double &theta){
+
+        if(omega == 0){
+            //Runge-Kutta integration
+            double dx = speed * dt * cos(this->theta + (omega * dt / 2));
+            double dy = speed * dt * sin(this->theta + (omega * dt / 2));
+            x += dx;
+            y += dy;
+            theta += dtheta;
+        }
+        else{
+            //Exact integration
+            double old_theta = theta;
+            theta += dtheta;
+            double dx = (speed / omega) * (sin(theta) - sin(old_theta));
+            double dy = (speed / omega) * (cos(theta) - cos(old_theta));
+            x += dx;
+            y -= dy;
+        }
     }
 
 public:
@@ -95,24 +124,25 @@ public:
         //init variables
         this->x = 0;
         this->y = 0;
-        this->theta = 0;
 
         //Subscrive to /speedsteer and publis to /odom
-        sub = nh.subscribe("speedsteer", 10, &Odometer::odom_callback, this);
-        pub = nh.advertise<nav_msgs::Odometry>("odom", 10);
+        this->sub = nh.subscribe("speedsteer", 10, &Odometer::odom_callback, this);
+        this->pub = nh.advertise<nav_msgs::Odometry>("odom", 10);
+
+        this->private_nh = ros::NodeHandle("~");
 
         //load parameters
-        nh.param("wheelbase", this->wheelbase, 1.765);
-        nh.param("scale_factor", this->scale_factor, 32);
+        this->private_nh.param("wheelbase", this->wheelbase, 1.765);
+        this->private_nh.param("scale_factor", this->scale_factor, 32);
+        this->private_nh.param("steer_threshold", this->steer_threshold, 0.02);
+        this->private_nh.param("speed_threshold", this->speed_threshold, 0.1);
+        this->nh.param("starting_gps_theta", this->theta, 1.474981);
     }
    
 };
 
 
-
 int main(int argc, char **argv){
-
-
     ros::init(argc, argv, "odometer");
     Odometer odometer;
     ros::spin();
